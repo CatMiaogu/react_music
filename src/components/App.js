72502fd90656eaeb74b8727cf108eef65813287c
zhaotao <1016489713@qq.com>
2018-02-06 21:30:44 +0800

    歌手页面的开发

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Redirect, NavLink} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect, NavLink} from "react-router-dom"
 import Recommend from "./recommend/Recommend"
 import Ranking from "./ranking/Ranking"
+import SingerList from "./singer/SingerList"
 import Search from "./search/Search"
-
-import logo from "../assets/imgs/logo.png";
-import '../assets/stylus/reset.styl'
-import './App.styl';
-import "../assets/stylus/font.styl"
-// import Player from "../containers/Player"
 import MusicPlayer from "./play/MusicPlayer"
+
+import logo from "../assets/imgs/logo.png"
+import '../assets/stylus/reset.styl'
+import "../assets/stylus/font.styl"
+import './App.styl'
 
 class App extends React.Component {
     render() {
@@ -17,7 +17,7 @@ class App extends React.Component {
             <Router>
                 <div className="app">
                     <header className="app-header">
-                        <img src={logo} className="app-logo" alt="logo"/>
+                        <img src={logo} className="app-logo" alt="logo" />
                         <h1 className="app-title">Mango Music</h1>
                     </header>
                     <div className="music-tab">
@@ -27,25 +27,33 @@ class App extends React.Component {
                             </NavLink>
                         </div>
                         <div className="tab-item">
-                            <NavLink to="./ranking" className="nav-link">
+                            <NavLink to="/ranking" className="nav-link">
                                 <span>排行榜</span>
                             </NavLink>
                         </div>
                         <div className="tab-item">
-                            <NavLink to="./search" className="nav-link">
+                            <NavLink to="/singer" className="nav-link">
+                                <span>歌手</span>
+                            </NavLink>
+                        </div>
+                        <div className="tab-item">
+                            <NavLink to="/search" className="nav-link">
                                 <span>搜索</span>
                             </NavLink>
                         </div>
                     </div>
                     <div className="music-view">
                         {/*
-                         switch组建用来选择最近的一个路由，否则最后一个没有指定path的路由也会显示
-                         Redirect重定向到列表页*/}
+                         Switch组件用来选择最近的一个路由，否则最后一个没有指定path的路由也会显示
+                         Redirect重定向到列表页
+                         */}
                         <Switch>
-                            <Route path="/recommend" component={Recommend}/>
-                            <Route path="/ranking" component={Ranking}/>
-                            <Route path="/search" component={Search}/>
-
+                            <Route path="/recommend" component={Recommend} />
+                            <Route path="/ranking" component={Ranking} />
+                            <Route path="/singer" component={SingerList} />
+                            <Route path="/search" component={Search} />
+                            <Redirect from="/" to="/recommend" />
+                            <Route component={Recommend} />
                         </Switch>
                     </div>
                     <MusicPlayer/>
---
+++ src/containers/Singer.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tzhao on 2018/2/6.
+ */
+import {connect} from "react-redux"
+import {showPlayer, changeSong, setSongs} from "../redux/actions"
+import Singer from "../components/singer/Singer"
+
+const mapDispatchToProps = (dispatch) => ({
+    showMusicPlayer: (status) => {
+        dispatch(showPlayer(status));
+    },
+    changeCurrentSong: (song) => {
+        dispatch(changeSong(song));
+    },
+    setSongs: (songs) => {
+        dispatch(setSongs(songs));
+    }
+});
+
+export default connect(null, mapDispatchToProps)(Singer)
